7262fc3e0779886fd900a07d7331fc0fdfc7b0a6
alienzhou <alienzhou16@163.com>
2019-04-14 18:56:27 +0800

    feat(init):

--- src/util/camel.ts
+++
@@ -1,7 +0,0 @@
-/**
- * convert dash-joined string to camel case
- */
-
-export default (str: string): string => (
-    str.split('-').reduce((str, s, idx) => str + (idx === 0 ? s: s[0].toUpperCase() + s.slice(1)), '')
-);
