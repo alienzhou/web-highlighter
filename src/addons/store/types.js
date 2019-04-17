d62b22a5ca8c8682019da453630dc82e635e2582
alienzhou <alienzhou16@163.com>
2019-04-17 22:25:31 +0800

    chore(remove): remove some files

--- src/addons/store/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var StoreType;
-(function (StoreType) {
-    StoreType["LOCAL"] = "localStorage";
-    StoreType["INDEX_DB"] = "indexDB";
-})(StoreType = exports.StoreType || (exports.StoreType = {}));
-//# sourceMappingURL=types.js.map
--- src/util/camel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * convert dash-joined string to camel case
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function (str) { return (str.split('-').reduce(function (str, s, idx) { return str + (idx === 0 ? s : s[0].toUpperCase() + s.slice(1)); }, '')); });
-//# sourceMappingURL=camel.js.map
