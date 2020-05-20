84649c1bc2f970d9f9cc5d133359184db160142a
zhouhongxuan <zhouhongxuan@kuaishou.com>
2020-05-20 17:56:17 +0800

    feat(hook): support remove tapped function in hooks; avoid add duplicate functions; add unit test

--- test/util.spec.ts
+++ test/util.spec.ts
@@ -3,7 +3,9 @@ import jsdomGlobal from 'jsdom-global';
 import HighlightSource from '../src/model/source/index';
 import sources from './fixtures/source.json';
 import Cache from '../src/data/cache';
+import Hook from '../src/util/hook';
 import { initDefaultStylesheet } from '../src/painter/style';
+import sinon from 'sinon';
 
 describe('Else Utils', function () {
     this.timeout(50000);
@@ -60,6 +62,105 @@ describe('Else Utils', function () {
             expect(document.querySelectorAll('style')).lengthOf(1);
             initDefaultStylesheet();
             expect(document.querySelectorAll('style')).lengthOf(1);
+        });
+    });
+
+    describe('Hook', () => {
+        it('should have the correct name', () => {
+            const hook = new Hook('test');
+            expect(hook.name).to.be.equal('test');
+        });
+
+        it('should be empty before tapping anything', () => {
+            const hook = new Hook();
+            expect(hook.isEmpty()).to.be.true;
+        });
+
+        it('should call only once for a tapped function', () => {
+            const spy: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const spy2: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const hook = new Hook();
+            hook.tap(spy);
+            hook.tap(spy2);
+            hook.call();
+
+            expect(spy.calledOnce).to.be.true;
+            expect(spy2.calledOnce).to.be.true;
+        });
+
+        it('should call tapped functions in the correct order', () => {
+            const seq = [];
+            const first = () => seq.push(1);
+            const second = () => seq.push(2);
+            const third = () => seq.push(3);
+            const hook = new Hook();
+            hook.tap(first);
+            hook.tap(second);
+            hook.tap(third);
+            hook.call();
+
+            expect(seq).to.deep.equal([1, 2, 3]);
+        });
+
+        it('should return a valid removing function when calling .tap', () => {
+            const spy: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const hook = new Hook();
+            const remove = hook.tap(spy);
+            remove();
+            hook.call();
+
+            expect(hook.isEmpty()).to.be.true;
+            expect(spy.callCount).to.be.equal(0);
+        });
+
+        it('should not tap the same function twice', () => {
+            const spy: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const hook = new Hook();
+            hook.tap(spy);
+            hook.tap(spy);
+            hook.call();
+
+            expect(spy.calledOnce).to.be.true;
+        });
+
+        it('should remove the tapped functions by calling .remove', () => {
+            const spy: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const hook = new Hook();
+            hook.tap(spy);
+            hook.remove(spy);
+            hook.call();
+            
+            expect(hook.isEmpty()).to.be.true;
+            expect(spy.callCount).to.be.equal(0);
+        });
+
+        it('should get the correct parameters when calling .call', () => {
+            const spy: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const spy2: sinon.SinonSpy<any[], any[]> = sinon.spy();
+            const hook = new Hook();
+            const p1 = 'test';
+            const p2 = [1];
+            hook.tap(spy);
+            hook.tap(spy2);
+            hook.call(p1, p2);
+            
+            expect(spy.args[0][0]).to.be.equal(p1);
+            expect(spy2.args[0][0]).to.be.equal(p1);
+            expect(spy.args[0][1]).to.be.equal(p2);
+            expect(spy2.args[0][1]).to.be.equal(p2);
+        });
+
+        it('should use the last tapped function\s return as the returned value', () => {
+            const hook = new Hook();
+            hook.tap(() => 1);
+            hook.tap(() => 2);
+            hook.tap(() => 3);
+            expect(hook.call()).to.be.equal(3);
+        });
+
+        it('should not throw error when removing a non-exist function', () => {
+            const hook = new Hook();
+            expect(() => hook.remove(() => {})).not.to.be.throw();
         });
     });
 });
